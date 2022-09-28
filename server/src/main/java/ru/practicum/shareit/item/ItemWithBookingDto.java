3484c182e063543bd3d322f75ae2518784be98e9
slava64 <slava0sva@gmail.com>
2022-09-28 18:27:43 +0400

    fix

--- server/src/main/java/ru/practicum/shareit/item/ItemWithBookingDto.java
+++ server/src/main/java/ru/practicum/shareit/item/ItemWithBookingDto.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Transient;
 import ru.practicum.shareit.booking.BookingItemDto;
 
 import java.io.Serializable;
