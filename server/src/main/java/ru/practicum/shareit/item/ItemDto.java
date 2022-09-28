8d83854a888a65ad51788a161128b7b06f630e5b
slava64 <slava0sva@gmail.com>
2022-09-28 18:16:44 +0400

    fix

--- server/src/main/java/ru/practicum/shareit/item/ItemDto.java
+++ server/src/main/java/ru/practicum/shareit/item/ItemDto.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.item;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Transient;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -16,5 +17,6 @@ public class ItemDto implements Serializable {
     private String description;
     private Boolean available;
     private Long requestId;
+    @Transient
     private ArrayList<CommentDto> comments;
 }
